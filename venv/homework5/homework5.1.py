fe9a91d12a303c7a35e8f087c6a0de553034856d
333ice333 <05KvrasD307>
2021-12-12 20:32:52 +0200

    test4

--- venv/homework5/homework5.1.py
+++ venv/homework5/homework5.1.py
@@ -1,1 +1,12 @@
-test
+import random
+
+size = 20
+random_size = 20
+result = []
+for _ in range(size):
+    result.append(random.randint(1, random_size))
+print(result)
+for i in list(result):
+    if result.count(i) > 1:
+        result.remove(i)
+print(result)
