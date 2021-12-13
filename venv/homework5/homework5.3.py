cdf28ee71c61f59a508440b30e3e25f72e95d7d9
333ice333 <05KvrasD307>
2021-12-13 20:41:31 +0200

    update  5.3

--- venv/homework5/homework5.3.py
+++ venv/homework5/homework5.3.py
@@ -1,1 +1,21 @@
-test
+import random
+
+size = 5
+random_size = 5
+result = []
+for _ in range(size):
+    result.append(random.randint(1, random_size))
+print(result)
+
+
+size_2 = 5
+random_size_2 = 5
+result_2 = []
+for _ in range(size_2):
+    result_2.append(random.randint(1, random_size_2))
+print(result_2)
+
+
+result_3=list(set(result)-set(result_2))
+print (result_3)
+
