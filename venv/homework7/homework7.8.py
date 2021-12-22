f959d7315ac7608c1d2f0b21f0301a6a80cd7df5
333ice333 <05KvrasD307>
2021-12-22 22:22:29 +0200

    add 7.8

--- venv/homework7/homework7.8.py
+++
@@ -1,7 +0,0 @@
-a = [int(i) for i in input("Введите числа через пробел ").split()]
-counter = 0
-for i in range(1, len(a) - 1):
-    # о боги, разве так можно писать?
-    if a[i - 1] < a[i] > a[i + 1]:
-        counter += 1
-print(counter)
